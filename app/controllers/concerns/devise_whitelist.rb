4014d780222821d1be16aae157944f3915de3d44
Tom Brennan <tommybrennan@gmail.com>
2018-09-01 11:14:28 -0400

    added devise whitelist concern

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,12 @@
+module DeviseWhitelist # Name must match file name Initial cap for first word and all that are preceeed by an underscore
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+end
